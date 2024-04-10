e1a47fd3b0b65913a1fbca8d35e24d4f878fac07
barun <barunps3@gmail.com>
2024-04-10 09:42:09 +0200

    made changes to initlua

---
+++ handmade/nvim/init.lua
@@ -0,0 +1,21 @@
+-- Set Options
+vim.opt.nu = true
+vim.opt.clipboard:append{"unnamedplus"}
+vim.opt.history = 50
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+
+-- Colorscheme
+vim.cmd.colorscheme("default")
+
+-- Keybindings
+vim.g.mapleader = " "
+vim.keymap.set('n', 'noh', ':nohls<CR>')
+vim.keymap.set('n', '<Leader>w', ':w<CR>')
+vim.keymap.set('n', '<Leader>J', '5j')
+vim.keymap.set('n', '<Leader>K', '5k')
+vim.keymap.set('n', '<Leader><Backspace>', ':Rex<CR>')
+vim.keymap.set('n', '<Leader>f', ':edit .<CR>')
+vim.keymap.set('n', 'rnu', ':set rnu!<CR>')
+
