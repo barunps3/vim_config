8dbe6585141d4c34bad796f136653b4bb7053b4e
barun <barunps3@gmail.com>
2024-04-14 00:56:43 +0200

    fixed comment

--- handmade/nvim/ftplugin/python.lua
+++ handmade/nvim/ftplugin/python.lua
@@ -3,9 +3,9 @@ local root_dirname = vim.fs.dirname(vim.fs.find({
     'setup.py',
     'requirements.txt'}, { upward = true })[1])
 
-print(string.format("Python Project Dir:", root_dirname))
+print(string.format("Python Project Dir:%s", root_dirname))
 
 vim.lsp.start({
     name = "pyright",
     cmd = { "pyright-langserver", "--stdio" },
-    root_dir = nil})
+    root_dir = root_dirname})
