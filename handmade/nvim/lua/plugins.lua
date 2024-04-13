281ab2b00a7cb6a1e23dd1d03478e8a52f492bf8
Barun Mazumdar <barun.mazumdar@cariad.technology>
2024-04-13 23:34:03 +0200

    saved nvim-lsp config

---
+++ handmade/nvim/lua/plugins.lua
@@ -0,0 +1,11 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd("packadd packer.nvim")
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Neovim lspconfig
+  use 'neovim/nvim-lspconfig'
+
+end)
